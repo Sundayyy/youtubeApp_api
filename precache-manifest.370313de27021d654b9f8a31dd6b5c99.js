51991a27a0f8c18d094abb350d5ac40273acc2bb
Sundayss <minhmanitobss09@gmail.com>
2020-05-04 16:26:46 +0700

    Updates

---
+++ precache-manifest.370313de27021d654b9f8a31dd6b5c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566f24bf27878a3238e0b99aa5034bcd",
+    "url": "/React-example/youtubeApp_api/index.html"
+  },
+  {
+    "revision": "f71b15deff7e7bc8c1e2",
+    "url": "/React-example/youtubeApp_api/static/css/main.32c64876.chunk.css"
+  },
+  {
+    "revision": "b0a569ebe2b61fb2a5ba",
+    "url": "/React-example/youtubeApp_api/static/js/2.163f335d.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/React-example/youtubeApp_api/static/js/2.163f335d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71b15deff7e7bc8c1e2",
+    "url": "/React-example/youtubeApp_api/static/js/main.b72cd586.chunk.js"
+  },
+  {
+    "revision": "9d359eb67e4ed9844e7b",
+    "url": "/React-example/youtubeApp_api/static/js/runtime-main.08f4d0e6.js"
+  }
+]);
